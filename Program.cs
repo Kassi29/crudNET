44ca71ead7ddf1256fe3824789f224d484979fb2
Kass Badani <username@users.noreply.github.com>
2025-02-03 11:56:59 -0400

    Add DbInitializer with test data and verify creation in Program.cs

--- Program.cs
+++ Program.cs
@@ -13,13 +13,28 @@ builder.Services.AddDbContext<SchoolContext>(options =>
 );
 builder.Services.AddControllersWithViews();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+//Ensure db is created
+using (var scope = app.Services.CreateScope()) {
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<SchoolContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception ex) { 
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error ocurred while initializing the db.");
+    }
 }
+
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
